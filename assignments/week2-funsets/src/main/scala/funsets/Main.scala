4a22ccc2128d767651c422e8baa96b7ae1cfecb6
N T R Phani Kumar <ntrphanikumar@gmail.com>
2016-11-11 20:32:51 +0530

    Completed week2 assignment with 10/10 score

--- assignments/week2-funsets/src/main/scala/funsets/Main.scala
+++ assignments/week2-funsets/src/main/scala/funsets/Main.scala
@@ -2,5 +2,12 @@ package funsets
 
 object Main extends App {
   import FunSets._
-  println(contains(singletonSet(1), 1))
+  val oneToTen: Set = x => Range(1, 11).contains(x)
+  val fiveToFifteen: Set = x=> Range(5,16).contains(x)
+
+  println(union(oneToTen, fiveToFifteen)(1), union(oneToTen, fiveToFifteen)(5), union(oneToTen, fiveToFifteen)(15),union(oneToTen, fiveToFifteen)(16))
+
+  println(map(oneToTen, x=> x*2)(10))
+
+//  println(FunSets.toString(oneToTen))
 }
